bc57c283d85daaa88286ca93ee4e1500aba856f7
Luke <ljfolns@gmail.com>
2026-08-19 22:40:22 -0400

    exercise 12

--- src/exercise12.ts
+++ src/exercise12.ts
@@ -1,3 +1,12 @@
+export type RemoteUser = {
+  id: number,
+  name: string,
+  email: string
+}
+
 export async function fetchUserEmails(): Promise<string[]> {
-  return [];
+  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+  // @ts-ignore
+  const users: RemoteUser[] = await (await fetch('https://jsonplaceholder.typicode.com/users')).json();
+  return users.map((user) => user.email)
 }
