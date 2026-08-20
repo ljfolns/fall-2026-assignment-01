fa1e8dcf80452a5a44eff8a58f3e84b97fd7a105
Luke <ljfolns@gmail.com>
2026-08-19 23:00:08 -0400

    exercise 14

--- src/exercise14.ts
+++ src/exercise14.ts
@@ -1,5 +1,21 @@
-export type PostItem = {};
+export type PostItem = {
+  id: number,
+  title: string,
+  body: string
+};
 
 export async function fetchPostBatch(postIds: number[]): Promise<PostItem[]> {
-  return [];
+  const promises: Promise<Response>[] = postIds.map((id) => {
+    return fetch(`https://jsonplaceholder.typicode.com/posts/${id}`);
+  });
+  const done: Response[] = await Promise.all(promises);
+
+  const mapped: Promise<PostItem>[] = done.map(async (map: Response): Promise<PostItem> => {
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-ignore
+    return await map.json();
+  });
+
+  // i think this is overengineered
+  return await Promise.all(mapped);
 }
