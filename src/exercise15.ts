131119ea0ea08fab89b95a1e005d9308988c1834
Luke <ljfolns@gmail.com>
2026-08-20 11:09:48 -0400

    exercise 15

--- src/exercise15.ts
+++ src/exercise15.ts
@@ -1,6 +1,33 @@
+import fs from 'fs';
+
+export type ContentSummary = {
+  email: string;
+  postId: number,
+  id: number,
+  commenterEmail: string
+}
+
 export async function processCommentsPipeline(
   targetPostId: number,
   outputPath: string,
 ): Promise<number> {
-  return 0;
+  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+  // @ts-ignore
+  let cs: Pick<ContentSummary>[] = await (
+    await fetch(
+      `https://jsonplaceholder.typicode.com/posts/${targetPostId}/comments`,
+    )
+  ).json();
+
+  cs = cs.filter((c) => {
+    return !c.email.includes(".org")
+  });
+  cs.map((c) => {
+    c.commenterEmail = c.email;
+    delete c.body
+  });
+
+  await fs.promises.writeFile(outputPath, JSON.stringify(cs));
+
+  return cs.length;
 }
