8ce0e5d780208b1234db88e5605381b5eebbaa87
Luke <ljfolns@gmail.com>
2026-08-19 22:37:11 -0400

    exercise 11

--- src/exercise11.ts
+++ src/exercise11.ts
@@ -1,4 +1,11 @@
+import fs from 'fs';
+
 export async function logStatusToFile(
   filePath: string,
   statusMessage: string,
-): Promise<void> {}
+): Promise<void> {
+  await fs.promises.appendFile(
+    filePath,
+    statusMessage + new Date(Date.now()).toISOString() + "\n"
+  );
+}
