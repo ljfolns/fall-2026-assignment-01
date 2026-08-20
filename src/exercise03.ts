98f4e7c2727e1e3e577c8add46f4c3adf3e10eef
Luke <ljfolns@gmail.com>
2026-08-19 21:58:19 -0400

    exercise 3

--- src/exercise03.ts
+++ src/exercise03.ts
@@ -1,5 +1,15 @@
 export function getInventoryValue(
   inventory: Array<[string, number, number]>,
 ): number {
-  return 0;
+  return inventory.filter((item: [string, number, number]): boolean => {
+    if (typeof item == 'string') return false;
+    if (typeof item == 'undefined') return false;
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-ignore
+    return item.at(1) > 5;
+  }).reduce((acc: number, curr: [string, number, number]): number => {
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-ignore
+    return acc + curr.at(1) * curr.at(2);
+  }, 0);
 }
