cd35642fa0e47d60da97506b1644d69f29d59c5f
Luke <ljfolns@gmail.com>
2026-08-20 10:58:31 -0400

    exercise 8

--- src/exercise08.ts
+++ src/exercise08.ts
@@ -4,10 +4,22 @@ export type EventMap = {
 };
 
 export class SimpleEventEmitter<T extends EventMap> {
+  private events: {
+    [K in keyof T]?: Array<(data: T[K]) => void>;
+  } = {};
+
   public on<K extends keyof T>(
     eventName: K,
     callback: (data: T[K]) => void,
-  ): void {}
+  ): void {
+    const handlers = this.events[eventName] ?? [];
+    handlers.push(callback);
+    this.events[eventName] = handlers;
+  }
 
-  public emit<K extends keyof T>(eventName: K, data: T[K]): void {}
+  public emit<K extends keyof T>(eventName: K, data: T[K]): void {
+    this.events[eventName]?.forEach((callback) => {
+      callback(data);
+    });
+  }
 }
