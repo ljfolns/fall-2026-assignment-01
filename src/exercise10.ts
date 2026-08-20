f735ae8e3ad28d30ac6745898d5b7e415240b2a8
Luke <ljfolns@gmail.com>
2026-08-19 22:28:22 -0400

    exercise 10

--- src/exercise10.ts
+++ src/exercise10.ts
@@ -1,15 +1,37 @@
-type UserAccount = {};
+type UserAccount = {
+  id: string,
+  createdAt: Date,
+  email: string,
+  passwordHash: string | undefined,
+  profile: {
+    bio: string,
+    avatarUrl: string
+  }
+};
 
 export class UserRegistry {
+  private register: UserAccount[] = [];
+
   public registerUser(
     data: Omit<UserAccount, 'id' | 'createdAt'>,
   ): UserAccount {
-    return {};
+    const user: UserAccount = {
+      id: Date.now().toString(),
+      createdAt: new Date(Date.now()),
+      email: data.email,
+      passwordHash: data.passwordHash,
+      profile: data.profile
+    };
+    this.register.push(user);
+    return user;
   }
 
   public getUserView(
     id: string,
   ): Readonly<Pick<UserAccount, 'id' | 'email' | 'profile'>> | undefined {
-    return undefined;
+    const user: UserAccount = <UserAccount> this.register.find((u) => u.id == id);
+    user.passwordHash = undefined;
+    Object.freeze(user)
+    return user;
   }
 }
